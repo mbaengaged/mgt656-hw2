74ddbf36900f5d173f6d37eca36d9826dd5a46b8
Victor Padilla-Taylor <victor.padilla-taylor@yale.edu>
2015-01-27 23:23:17 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! This is part 1 of HW2');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('ok nightingale');
 });
 
 app.listen(process.env.PORT || 4000);
